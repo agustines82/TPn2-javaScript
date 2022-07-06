6c8a403a8b5f8d80ed94d3fb5e0621471b47c108
Agustin Sampayo <agustines82@gmail.com>
2022-07-06 20:29:24 -0300

    comentarios sobre el ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,2 @@
+//Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.Nota: usar confirm()
+//pienso: tengo que crear una nueva variable por cada prompt que se realice y guardar en ellas cada una de las frases. luego concatenar todo.
