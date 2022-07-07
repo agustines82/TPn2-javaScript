78e12060d7c88d38056da0e835db187d2751bdab
Agustin Sampayo <agustines82@gmail.com>
2022-07-07 17:12:44 -0300

    ejercicio 3 listo

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,2 +1,8 @@
 //Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.Nota: usar confirm()
-//pienso: tengo que crear una nueva variable por cada prompt que se realice y guardar en ellas cada una de las frases. luego concatenar todo.
+
+let cadenas = prompt("Ingrese una cadena de texto");
+document.write(cadenas + "-");
+while (confirm(cadenas) === true) {
+    cadenas = prompt("Ingrese una cadena de texto");
+    document.write(cadenas + "-");
+}
