335925f726534ca7b9039c9417097b2b249c6423
Agustin Sampayo <agustines82@gmail.com>
2022-07-14 19:46:43 -0300

    ejercicio 9 listo

---
+++ js/ejercicio9.js
@@ -0,0 +1,28 @@
+/*Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
+
+1
+2
+3
+4 (Múltiplo de 4)
+5-
+————————————————————-
+
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10
+*/
+for (let indice = 1; indice <= 500; indice++) {
+    document.write(indice);
+    if (indice % 4 === 0) {
+        document.write(" (multiplo de 4).");
+    }
+    if (indice % 9 === 0) {
+        document.write(" (multiplo de 9)");
+    }
+    document.write("<br>");
+    if (indice % 5 === 0) {
+        document.write("<hr>");
+    }
+}
